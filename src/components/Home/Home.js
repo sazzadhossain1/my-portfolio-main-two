8f7c0f6283560f7ba040fcb97bc91bcb27e779f9
sazzadhossain1 <sazzadtomal1@gmail.com>
2023-05-02 15:46:27 +0600

    start service section and react icone install

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -2,12 +2,14 @@ import React from "react";
 import "./Home.css";
 import Header from "../Header/Header";
 import Identity from "../Identity/Identity";
+import Services from "../Services/Services";
 
 const Home = () => {
   return (
     <div className="home-parent-div">
       <Header></Header>
       <Identity></Identity>
+      <Services></Services>
     </div>
   );
 };
---
+++ src/components/Services/Services.js
@@ -0,0 +1,19 @@
+import React from "react";
+import "./Services.css";
+
+const Services = () => {
+  return (
+    <div className="services-parent-div">
+      <div className="service-child-parent">
+        <h1 className="service-h1">My Services</h1>
+        <div className="provider-parent-div">
+          <div className="blank-div"></div>
+          <h3 className="provide-h3">What i provide</h3>
+          <div className="blank-div"></div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Services;
