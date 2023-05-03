f83652ca6f01b0e5ac2cd761330ddb820481f1f9
sazzadhossain1 <sazzadtomal1@gmail.com>
2023-05-03 16:31:02 +0600

    footer css style and responsiveness done

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -3,7 +3,7 @@ import "./Footer.css";
 
 const Footer = () => {
   return (
-    <footer className="footer p-10 bg-base-300 text-base-content footer-div">
+    <footer className=" p-10  text-base-content footer-div">
       <div className="mx-auto">
         <span className="footer-title">Services</span>
         <a className="link link-hover">Branding</a>
@@ -20,7 +20,7 @@ const Footer = () => {
       </div>
       <div className="mx-auto">
         <span className="footer-title">Social</span>
-        <div className="grid grid-flow-col gap-4">
+        <div className="grid grid-flow-col gap-4 mt-6">
           <a>
             <svg
               xmlns="http://www.w3.org/2000/svg"
