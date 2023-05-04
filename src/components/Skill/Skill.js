aaa02149398cefca55f3087f9fa644db180cafb7
sazzadhossain1 <sazzadtomal1@gmail.com>
2023-05-05 02:25:16 +0600

    add logo in the skill's cart

--- src/components/Skill/Skill.js
+++ src/components/Skill/Skill.js
@@ -17,7 +17,7 @@ const Skill = () => {
           data-aos="zoom-out"
           data-aos-duration="2000"
         >
-          <h1 className="percent-heading">85%</h1>
+          <h1 className="percent-heading">95%</h1>
           <div className="blank-white-div"></div>
           <div>
             <img
@@ -33,15 +33,79 @@ const Skill = () => {
           data-aos="zoom-out"
           data-aos-duration="2000"
         >
-          <h1 className="percent-heading">85%</h1>
+          <h1 className="percent-heading">90%</h1>
           <div className="blank-white-div"></div>
           <div>
             <img
               className="logo"
-              src="https://upload.wikimedia.org/wikipedia/commons/thumb/a/a8/AnantP%40html.png/600px-AnantP%40html.png?20211203125137"
+              src="https://upload.wikimedia.org/wikipedia/commons/thumb/8/80/AnantP%40css.png/600px-AnantP%40css.png?20211203125137"
               alt=""
             />
-            <p className="skill-name">HTML</p>
+            <p className="skill-name">CSS</p>
+          </div>
+        </div>
+        <div
+          className="percent-skill-cart-flex-div"
+          data-aos="zoom-out"
+          data-aos-duration="2000"
+        >
+          <h1 className="percent-heading">90% </h1>
+          <div className="blank-white-div"></div>
+          <div>
+            <img
+              className="logo"
+              src="https://upload.wikimedia.org/wikipedia/commons/thumb/b/b2/Bootstrap_logo.svg/512px-Bootstrap_logo.svg.png?20210507000024"
+              alt=""
+            />
+            <p className="skill-name">Bootstrap</p>
+          </div>
+        </div>
+        <div
+          className="percent-skill-cart-flex-div"
+          data-aos="zoom-out"
+          data-aos-duration="2000"
+        >
+          <h1 className="percent-heading">95%</h1>
+          <div className="blank-white-div"></div>
+          <div>
+            <img
+              className="logo"
+              src="https://vasterra.com/blog/wp-content/uploads/2021/08/Tailwind-img.png"
+              alt=""
+            />
+            <p className="skill-name">Tailwind</p>
+          </div>
+        </div>
+        <div
+          className="percent-skill-cart-flex-div"
+          data-aos="zoom-out"
+          data-aos-duration="2000"
+        >
+          <h1 className="percent-heading">75%</h1>
+          <div className="blank-white-div"></div>
+          <div>
+            <img
+              className="logo"
+              src="https://www.freepnglogos.com/uploads/javascript/js-circle-black-design-logo-30.png"
+              alt=""
+            />
+            <p className="skill-name">JavaScript</p>
+          </div>
+        </div>
+        <div
+          className="percent-skill-cart-flex-div"
+          data-aos="zoom-out"
+          data-aos-duration="2000"
+        >
+          <h1 className="percent-heading">80%</h1>
+          <div className="blank-white-div"></div>
+          <div>
+            <img
+              className="logo"
+              src="https://ih1.redbubble.net/image.1583754649.5587/poster,504x498,f8f8f8-pad,600x600,f8f8f8.jpg"
+              alt=""
+            />
+            <p className="skill-name">React.Js</p>
           </div>
         </div>
         <div
@@ -54,10 +118,10 @@ const Skill = () => {
           <div>
             <img
               className="logo"
-              src="https://upload.wikimedia.org/wikipedia/commons/thumb/a/a8/AnantP%40html.png/600px-AnantP%40html.png?20211203125137"
+              src="https://justinmahar.gallerycdn.vsassets.io/extensions/justinmahar/react-bootstrap-snippets/2.1.0/1674710651199/Microsoft.VisualStudio.Services.Icons.Default"
               alt=""
             />
-            <p className="skill-name">HTML</p>
+            <p className="skill-name">React Bootstrap</p>
           </div>
         </div>
         <div
@@ -65,15 +129,15 @@ const Skill = () => {
           data-aos="zoom-out"
           data-aos-duration="2000"
         >
-          <h1 className="percent-heading">85%</h1>
+          <h1 className="percent-heading">95%</h1>
           <div className="blank-white-div"></div>
           <div>
             <img
               className="logo"
-              src="https://upload.wikimedia.org/wikipedia/commons/thumb/a/a8/AnantP%40html.png/600px-AnantP%40html.png?20211203125137"
+              src="https://images.opencollective.com/daisyui/d1eb928/logo/256.png"
               alt=""
             />
-            <p className="skill-name">HTML</p>
+            <p className="skill-name">DaisyUi</p>
           </div>
         </div>
         <div
@@ -81,15 +145,15 @@ const Skill = () => {
           data-aos="zoom-out"
           data-aos-duration="2000"
         >
-          <h1 className="percent-heading">85%</h1>
+          <h1 className="percent-heading">90%</h1>
           <div className="blank-white-div"></div>
           <div>
             <img
               className="logo"
-              src="https://upload.wikimedia.org/wikipedia/commons/thumb/a/a8/AnantP%40html.png/600px-AnantP%40html.png?20211203125137"
+              src="https://images.opencollective.com/swiper/21f926c/logo/256.png"
               alt=""
             />
-            <p className="skill-name">HTML</p>
+            <p className="skill-name">Swiperjs</p>
           </div>
         </div>
         <div
@@ -97,15 +161,15 @@ const Skill = () => {
           data-aos="zoom-out"
           data-aos-duration="2000"
         >
-          <h1 className="percent-heading">85%</h1>
+          <h1 className="percent-heading">80%</h1>
           <div className="blank-white-div"></div>
           <div>
             <img
               className="logo"
-              src="https://upload.wikimedia.org/wikipedia/commons/thumb/a/a8/AnantP%40html.png/600px-AnantP%40html.png?20211203125137"
+              src="https://cdn.iconscout.com/icon/free/png-512/free-firebase-3521427-2944871.png?f=avif&w=256"
               alt=""
             />
-            <p className="skill-name">HTML</p>
+            <p className="skill-name">Firebase</p>
           </div>
         </div>
         <div
@@ -113,15 +177,15 @@ const Skill = () => {
           data-aos="zoom-out"
           data-aos-duration="2000"
         >
-          <h1 className="percent-heading">85%</h1>
+          <h1 className="percent-heading">90%</h1>
           <div className="blank-white-div"></div>
           <div>
             <img
               className="logo"
-              src="https://upload.wikimedia.org/wikipedia/commons/thumb/a/a8/AnantP%40html.png/600px-AnantP%40html.png?20211203125137"
+              src="https://fiverr-res.cloudinary.com/images/t_main1,q_auto,f_auto,q_auto,f_auto/gigs/140609303/original/e2c6bf3c4e4d5f31069904d150573ae929505cd7/convert-figma-to-html.png"
               alt=""
             />
-            <p className="skill-name">HTML</p>
+            <p className="skill-name">Figma to HTML</p>
           </div>
         </div>
         <div
@@ -129,79 +193,15 @@ const Skill = () => {
           data-aos="zoom-out"
           data-aos-duration="2000"
         >
-          <h1 className="percent-heading">85%</h1>
+          <h1 className="percent-heading">90%</h1>
           <div className="blank-white-div"></div>
           <div>
             <img
               className="logo"
-              src="https://upload.wikimedia.org/wikipedia/commons/thumb/a/a8/AnantP%40html.png/600px-AnantP%40html.png?20211203125137"
+              src="https://global-uploads.webflow.com/618fa90c201104b94458e1fb/64118c6b4ca1644a6d0f895d_Best-Figma-plugins-for-React-An-In-depth-Analysis_Main-Image.jpg"
               alt=""
             />
-            <p className="skill-name">HTML</p>
-          </div>
-        </div>
-        <div
-          className="percent-skill-cart-flex-div"
-          data-aos="zoom-out"
-          data-aos-duration="2000"
-        >
-          <h1 className="percent-heading">85%</h1>
-          <div className="blank-white-div"></div>
-          <div>
-            <img
-              className="logo"
-              src="https://upload.wikimedia.org/wikipedia/commons/thumb/a/a8/AnantP%40html.png/600px-AnantP%40html.png?20211203125137"
-              alt=""
-            />
-            <p className="skill-name">HTML</p>
-          </div>
-        </div>
-        <div
-          className="percent-skill-cart-flex-div"
-          data-aos="zoom-out"
-          data-aos-duration="2000"
-        >
-          <h1 className="percent-heading">85%</h1>
-          <div className="blank-white-div"></div>
-          <div>
-            <img
-              className="logo"
-              src="https://upload.wikimedia.org/wikipedia/commons/thumb/a/a8/AnantP%40html.png/600px-AnantP%40html.png?20211203125137"
-              alt=""
-            />
-            <p className="skill-name">HTML</p>
-          </div>
-        </div>
-        <div
-          className="percent-skill-cart-flex-div"
-          data-aos="zoom-out"
-          data-aos-duration="2000"
-        >
-          <h1 className="percent-heading">85%</h1>
-          <div className="blank-white-div"></div>
-          <div>
-            <img
-              className="logo"
-              src="https://upload.wikimedia.org/wikipedia/commons/thumb/a/a8/AnantP%40html.png/600px-AnantP%40html.png?20211203125137"
-              alt=""
-            />
-            <p className="skill-name">HTML</p>
-          </div>
-        </div>
-        <div
-          className="percent-skill-cart-flex-div"
-          data-aos="zoom-out"
-          data-aos-duration="2000"
-        >
-          <h1 className="percent-heading">85%</h1>
-          <div className="blank-white-div"></div>
-          <div>
-            <img
-              className="logo"
-              src="https://upload.wikimedia.org/wikipedia/commons/thumb/a/a8/AnantP%40html.png/600px-AnantP%40html.png?20211203125137"
-              alt=""
-            />
-            <p className="skill-name">HTML</p>
+            <p className="skill-name">Figma to React</p>
           </div>
         </div>
       </div>
