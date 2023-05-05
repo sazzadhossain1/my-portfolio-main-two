681678fd7cdc4f8ceaec001f85511b2842c2994e
sazzadhossain1 <sazzadtomal1@gmail.com>
2023-05-05 21:47:22 +0600

    add react font awsome icon in the input name field

--- src/components/SignUp/SignUp.js
+++ src/components/SignUp/SignUp.js
@@ -2,6 +2,7 @@ import React from "react";
 import "./SignUp.css";
 import Header from "../Header/Header";
 import { Link } from "react-router-dom";
+import { FaAdn, FaVoicemail } from "react-icons/fa";
 
 const SignUp = () => {
   return (
@@ -14,13 +15,16 @@ const SignUp = () => {
           <form className="card-body  mx-auto">
             <div className="form-control mx-auto">
               <label className="label">
-                <span className="label-text">Your Email</span>
+                <span className="label-text">Your Name</span>
               </label>
               <label className="input-group">
-                <span className="input-left">Email</span>
+                <span className="input-left">
+                  <FaAdn></FaAdn>
+                </span>
                 <input
                   type="text"
-                  placeholder="info@site.com"
+                  name="name"
+                  placeholder="Your Name"
                   className=" input-field"
                 />
               </label>
@@ -32,7 +36,8 @@ const SignUp = () => {
               <label className="input-group">
                 <span className="input-left">Email</span>
                 <input
-                  type="text"
+                  type="email"
+                  name="email"
                   placeholder="info@site.com"
                   className=" input-field"
                 />
@@ -40,12 +45,13 @@ const SignUp = () => {
             </div>
             <div className="form-control mx-auto">
               <label className="label">
-                <span className="label-text">Your Email</span>
+                <span className="label-text">Password</span>
               </label>
               <label className="input-group">
-                <span className="input-left">Email</span>
+                <span className="input-left">Password</span>
                 <input
-                  type="text"
+                  type="password"
+                  name="password"
                   placeholder="info@site.com"
                   className=" input-field"
                 />
@@ -53,12 +59,13 @@ const SignUp = () => {
             </div>
             <div className="form-control mx-auto">
               <label className="label">
-                <span className="label-text">Your Email</span>
+                <span className="label-text">Confirm Password</span>
               </label>
               <label className="input-group">
-                <span className="input-left">Email</span>
+                <span className="input-left">Confirm</span>
                 <input
-                  type="text"
+                  type="password"
+                  name="confirmPassword"
                   placeholder="info@site.com"
                   className=" input-field"
                 />
