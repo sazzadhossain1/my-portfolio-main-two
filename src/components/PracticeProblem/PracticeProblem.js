10104bef0dfaf2f2bd9add3d5dd3cff288c14e82
sazzadhossain1 <sazzadtomal1@gmail.com>
2023-05-07 19:17:50 +0600

    set coming soon photo in practice problem route

--- src/components/PracticeProblem/PracticeProblem.js
+++ src/components/PracticeProblem/PracticeProblem.js
@@ -6,8 +6,14 @@ const PracticeProblem = () => {
   return (
     <div className="practice-problem-parent-div">
       <Header></Header>
-      <h1> there is a lot of problems you can PracticeProblem</h1>
-      <img src="https://wallpaperaccess.com/full/2588788.jpg" alt="" />
+      <div className="photo-div">
+        <h1> there is a lot of problems you can PracticeProblem</h1>
+        <img
+          className="coming-soon-photo"
+          src="https://wallpaperaccess.com/full/2588788.jpg"
+          alt=""
+        />
+      </div>
     </div>
   );
 };
