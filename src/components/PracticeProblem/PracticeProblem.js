5b6af9e9ae9f08138f71ca1bf0de1e293db52256
sazzadhossain1 <sazzadtomal1@gmail.com>
2023-05-07 09:52:10 +0600

    background img and comming soon photo set in practice problems route

--- src/components/PracticeProblem/PracticeProblem.js
+++ src/components/PracticeProblem/PracticeProblem.js
@@ -1,11 +1,13 @@
 import React from "react";
 import Header from "../Header/Header";
+import "./PracticeProblem.css";
 
 const PracticeProblem = () => {
   return (
-    <div>
+    <div className="practice-problem-parent-div">
       <Header></Header>
       <h1> there is a lot of problems you can PracticeProblem</h1>
+      <img src="https://wallpaperaccess.com/full/2588788.jpg" alt="" />
     </div>
   );
 };
